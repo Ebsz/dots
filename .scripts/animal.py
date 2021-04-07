a97901dd0d7ea76fceab8d03e679266458e21dad
Einar Sønju <einarsonju@gmail.com>
2021-04-07 22:02:35 +0200

    tmux: name sessions after cool animals

---
+++ .scripts/animal.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#
+# Outputs the name of an animal
+
+import random
+
+animals = ["tiger", "monkey", "shark", "lion", "panda", "wolf", "dolphin", "koala", "jaguar"]
+
+print(random.choice(animals))
+
